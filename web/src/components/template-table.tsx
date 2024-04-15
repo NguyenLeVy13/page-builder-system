88c915c6cebd261a96165ba61886ae888ac19120
NguyenLeVy13 <2014509@dlu.edu.vn>
2024-04-15 23:20:40 +0700

    Rename file

--- web/src/components/template-table.tsx
+++ web/src/components/template-table.tsx
@@ -149,10 +149,10 @@ function TemplateTable() {
     <div className="w-full">
       <div className="flex items-center py-4">
         <Input
-          placeholder="Filter emails..."
-          value={(table.getColumn("email")?.getFilterValue() as string) ?? ""}
+          placeholder="Search"
+          value={(table.getColumn("title")?.getFilterValue() as string) ?? ""}
           onChange={(event) =>
-            table.getColumn("email")?.setFilterValue(event.target.value)
+            table.getColumn("title")?.setFilterValue(event.target.value)
           }
           className="max-w-sm"
         />

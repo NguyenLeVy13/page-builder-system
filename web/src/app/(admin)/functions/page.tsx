a82a33305c09ca27af819f5da3c8608d896c649e
NguyenLeVy13 <2014509@dlu.edu.vn>
2024-04-20 19:28:15 +0700

    Update page

--- web/src/app/(admin)/functions/page.tsx
+++ web/src/app/(admin)/functions/page.tsx
@@ -1,3 +1,5 @@
+import React from 'react'
+
 function Functions() {
   return (
     <div>Functions</div>
--- web/src/app/(admin)/menu/page.tsx
+++ web/src/app/(admin)/menu/page.tsx
@@ -1,3 +1,5 @@
+import React from 'react'
+
 function Menu() {
   return (
     <div>Menu</div>
